49d0861126bc43b010879d1af23f2ca97d4021ea
Bobby Shunk <Bobby@macbook-pro-5.attlocal.net>
2017-07-04 14:25:22 -0500

    establish an input box in the index.html and work on grabbing the input and manipulating it in JS

--- coins.js
+++ coins.js
@@ -1,25 +1,24 @@
 console.log("ahoy mate!")
-
 // Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
-//   Input: 0.67
-//   Output:
-//   {
-//     quarters: 2,
-//     dimes: 1,
-//     nickels: 1,
-//     pennies: 2
-//   }
+  // Input: 0.67
+  // Output:
+  // {
+  //   quarters: 2,
+  //   dimes: 1,
+  //   nickels: 1,
+  //   pennies: 2
+  // }
+var dollarAmountDOM = document.getElementById("dollarAmountDOM");
 
-
-
-function coinCounter () {
+function coinCounter (money) {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
-  
   coinPurse.quarters = 0;
-
-  return coinPurse;
-}
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
+	return coinPurse;
+};
 
 var coins = coinCounter()
-console.log();
+console.log("coins", coins);
